16a305a57554ea44a6845a8842c85f63e9943765
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-04-10 11:24:35 +0600

    Header section added

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='flex gap-5 items-center bg-[#F9F9FF]'>
+            <div className='w-[40%]'>
+                <h1 className='text-[57px] font-bold leading-[4rem]'>One Step Closer To Your <span className='text-[#9873FF]'>Dream Job</span></h1>
+                <p className='mt-5 mb-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <Link className='btn-primary'>Get started</Link>
+            </div>
+            <div>
+                <img className='h-[600px] w-full' src="/public/All Images/P3OLGJ1 copy 1.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
